7dbfbad820ba4e0753c894642b0403008a25a61a
mdevine96 <102774014+mdevine96@users.noreply.github.com>
2026-02-02 22:33:00 -0500

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,7 @@
 const API_BASE = import.meta.env.VITE_API_URL;
 
 export async function submitGuess(guessId) {
+  if (!API_BASE) console.error('VITE_API_URL not set!');
   const res = await fetch(`${API_BASE}/api/guess`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
